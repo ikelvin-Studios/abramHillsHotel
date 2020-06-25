70d3ea132ab0d2600833bb8472d28585da90d37c
Kelvin Morrison <kelvinninja1@gmail.com>
2020-06-25 19:30:25 +0000

    Initialized: Files

---
+++ footer.php
@@ -0,0 +1,66 @@
+
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Abram Hills Hotel</h2>
+              <p>Far far away, behind the mountains and hills.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Useful Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Reservations</a></li>
+                <li><a href="#" class="py-2 d-block">Rooms</a></li>
+                <li><a href="#" class="py-2 d-block">Restaurants</a></li>
+                <li><a href="#" class="py-2 d-block">Amenities</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Privacy</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Career</a></li>
+                <li><a href="#" class="py-2 d-block">About Us</a></li>
+                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                <li><a href="#" class="py-2 d-block">Services</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text"> St. Area, Koforidua, Eastern Region, Ghana/span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">(+233) Contact</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@abramhillshotel.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="tel:+233551838987" target="_blank">ikelvin Studios</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+    
+ 
---
+++ template.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$page = "pagename";
+
+?>
+
+  <!-- head -->
+<?php
+include('head.php');
+
+?>
+
+  <body>
+
+  
+    <!-- nav  header-->
+<?php
+include('header.php');
+?>
+
+
+<!-- contents -->
+//contents
+
+
+    <!-- footer -->
+<?php
+include('footer.php');
+?>
+    
+  
+
+  <!-- scripts-->
+ <?php
+ include('scripts.php');
+ 
+ ?>
+  
+    
+  </body>
+</html>
